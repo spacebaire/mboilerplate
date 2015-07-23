75601a26d755ce9cd3c95b33f681c3821dacd730
chuycepeda <chuycepeda@gmail.com>
2015-07-22 19:05:24 -0500

    dev: huge file simplification.

--- bp_includes/handlers.py
+++ bp_includes/handlers.py
@@ -1945,7 +1945,7 @@ class MaterializeSettingsProfileRequestHandler(BaseHandler):
         f = forms.SettingsProfileForm(self)
         return f
 
-class MaterializeSettingsHomeRequestHandler(BaseHandler):
+class MaterializeSettingsAddressRequestHandler(BaseHandler):
     """
         Handler for materialized settings home
     """
@@ -2008,7 +2008,7 @@ class MaterializeSettingsHomeRequestHandler(BaseHandler):
             else:
                 params['panels'] = 0
                 params['capacity'] = 250
-        return self.render_template('materialize/users/settings/home.html', **params)
+        return self.render_template('materialize/users/settings/address.html', **params)
 
     def post(self):
         """ Get fields from POST dict """
@@ -2588,157 +2588,6 @@ class MaterializeSettingsDeleteRequestHandler(BaseHandler):
     def form(self):
         return forms.DeleteAccountForm(self)
 
-class MaterializeFaqRequestHandler(BaseHandler):
-    """
-        Handler for materialized frequented asked questions
-    """
-    @user_required
-    def get(self):
-        """ returns simple html for a get request """
-        params, user_info = disclaim(self)
-        return self.render_template('materialize/users/starter/faq.html', **params)
-
-class MaterializeTouRequestHandler(BaseHandler):
-    """
-        Handler for materialized terms of use
-    """
-    @user_required
-    def get(self):
-        """ returns simple html for a get request """
-        params, user_info = disclaim(self)
-        return self.render_template('materialize/users/starter/tou.html', **params)
-
-class MaterializePrivacyRequestHandler(BaseHandler):
-    """
-        Handler for materialized privacy policy
-    """
-    @user_required
-    def get(self):
-        """ returns simple html for a get request """
-        params, user_info = disclaim(self)
-        return self.render_template('materialize/users/starter/privacy.html', **params)
-
-class MaterializeContactRequestHandler(BaseHandler):
-    """
-        Handler for materialized contact us
-    """
-    @user_required
-    def get(self):
-        """ returns simple html for a get request """
-        params, user_info = disclaim(self)
-        if self.user:
-            user_info = self.user_model.get_by_id(long(self.user_id))
-            if user_info.name or user_info.last_name:
-                self.form.name.data = user_info.name + " " + user_info.last_name
-            if user_info.email:
-                self.form.email.data = user_info.email
-        params['exception'] = self.request.get('exception')
-
-        return self.render_template('materialize/users/starter/contact.html', **params)
-
-    def post(self):
-        """ validate contact form """
-        if not self.form.validate():
-            _message = _(messages.post_error)
-            self.add_message(_message, 'danger')
-            return self.get()
-
-        import bp_includes.lib.i18n as i18n
-        from bp_includes.external import httpagentparser
-
-        remote_ip = self.request.remote_addr
-        city = i18n.get_city_code(self.request)
-        region = i18n.get_region_code(self.request)
-        country = i18n.get_country_code(self.request)
-        coordinates = i18n.get_city_lat_long(self.request)
-        user_agent = self.request.user_agent
-        exception = self.request.POST.get('exception')
-        name = self.form.name.data.strip()
-        email = self.form.email.data.lower()
-        message = self.form.message.data.strip()
-        template_val = {
-            "name": name,
-            "email": email,
-            "ip": remote_ip,
-            "city": city,
-            "region": region,
-            "country": country,
-            "coordinates": coordinates,
-            "message": message
-        }
-        try:
-            # parsing user_agent and getting which os key to use
-            # windows uses 'os' while other os use 'flavor'
-            ua = httpagentparser.detect(user_agent)
-            _os = ua.has_key('flavor') and 'flavor' or 'os'
-
-            operating_system = str(ua[_os]['name']) if "name" in ua[_os] else "-"
-            if 'version' in ua[_os]:
-                operating_system += ' ' + str(ua[_os]['version'])
-            if 'dist' in ua:
-                operating_system += ' ' + str(ua['dist'])
-
-            browser = str(ua['browser']['name']) if 'browser' in ua else "-"
-            browser_version = str(ua['browser']['version']) if 'browser' in ua else "-"
-
-            template_val = {
-                "name": name,
-                "email": email,
-                "ip": remote_ip,
-                "city": city,
-                "region": region,
-                "country": country,
-                "coordinates": coordinates,
-
-                "browser": browser,
-                "browser_version": browser_version,
-                "operating_system": operating_system,
-                "message": message
-            }
-        except Exception as e:
-            logging.error("error getting user agent info: %s" % e)
-
-        try:
-            subject = _("Alguien ha enviado un mensaje")
-            # exceptions for error pages that redirect to contact
-            if exception != "":
-                subject = "{} (Exception error: {})".format(subject, exception)
-
-            body_path = "emails/contact.txt"
-            body = self.jinja2.render_template(body_path, **template_val)
-
-            email_url = self.uri_for('taskqueue-send-email')
-            taskqueue.add(url=email_url, params={
-                'to': self.app.config.get('contact_recipient'),
-                'subject': subject,
-                'body': body,
-                'sender': self.app.config.get('contact_sender'),
-            })
-
-            message = _(messages.contact_success)
-            self.add_message(message, 'success')
-            return self.redirect_to('materialize-contact')
-
-        except (AttributeError, KeyError), e:
-            logging.error('Error sending contact form: %s' % e)
-            message = _(messages.post_error)
-            self.add_message(message, 'danger')
-            return self.redirect_to('materialize-contact')
-
-    @webapp2.cached_property
-    def form(self):
-        return forms.ContactForm(self)
-
-class MaterializeLicenseRequestHandler(BaseHandler):
-    """
-        Handler for materialized terms of use
-    """
-    @user_required
-    def get(self):
-        """ returns simple html for a get request """
-        params, user_info = disclaim(self)
-        return self.render_template('materialize/users/starter/license.html', **params)
-
 class MaterializeTutorialsRequestHandler(BaseHandler):
     """
         Handler for materialized terms of use
@@ -2747,7 +2596,7 @@ class MaterializeTutorialsRequestHandler(BaseHandler):
     def get(self):
         """ returns simple html for a get request """
         params, user_info = disclaim(self)
-        return self.render_template('materialize/users/starter/tutorials.html', **params)
+        return self.render_template('materialize/users/sections/tutorials.html', **params)
 
 
 
